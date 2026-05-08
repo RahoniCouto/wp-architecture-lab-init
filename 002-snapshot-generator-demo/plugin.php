8f35b686a374811c4b2cc5ab7ffc4722b418902a
RahoniCouto <RahoniDevCouto@gmail.com>
2026-05-07 21:41:07 -0300

    add bootstrap Plugin snapsgot Generator Demo

---
+++ 002-snapshot-generator-demo/plugin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Architecture Lab - Snapshot Generator Demo Basic
+ * Description: Plugin simples de geração de Snapshot basicoo, usa apenas PSR-4, DI e dados prê computados, não recomendado para alto trafego.
+ * Version: 0.1.0
+ * Author Vitor
+ */
+
+declare(strict_types=1);
+
+if( !defined('ABSPATH')) {
+    exit;
+}
+
+$autoload = __DIR__ . '/vendor/autoload.php';
+
+if(!file_exists($autoload)){
+    add_action('admin_noticias', static function (): void {
+        ?>
+            <div class="notice notice-error">
+                <p><?php echo esc_html__(
+                            'Composer autoload não foi encontrado. Run composer install.','architecture-lab'); 
+                    ?>
+                </p>
+            </div>
+        <?php
+    });
+
+    return;
+}
+
+require_once $autoload;
+
+ArchitectureLab\SnapshotGeneratorDemo\Plugin::init();
---
+++ 002-snapshot-generator-demo/src/Plugin.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace ArchitectureLab\SnapshotGeneratorDemo;
+
+final class Plugin {
+    public static function init(): void {
+        // Aqui irei adicionar as dependencias
+    }
+}
