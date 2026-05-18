a1e5134dd2b613ce0676061136c62934ca4c1d2c
RahoniCouto <RahoniDevCouto@gmail.com>
2026-05-18 14:08:09 -0300

    add snapshot requested domain event

---
+++ 006-event-dispatcher-demo/src/Events/SnapshotRequestedEvent.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace ArchitectureLab\EventDispatcherDemo\Events;
+
+final class SnapshotRequestedEvent {
+    public function __construct(
+        public readonly int $postId,
+        public readonly string $snapshotKey = 'latest-post'
+    ){}
+}
